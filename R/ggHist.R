3c5ad8fa6a849b737080074adc9929227bd394a2
hferg <henry.fergusongow@gmail.com>
2017-10-24 22:15:26 +0100

    Changes to description to remove reference to tidyverse (not used) and instead added (preemtpively added imports for phytools and geiger (which will be surely needed as I update functions forther along in the package).

--- R/ggHist.R
+++ R/ggHist.R
@@ -1,5 +1,5 @@
 #' ggHist
-#' 
+#'
 #' A function to plot a nice histogram - one that uses ggplot2
 #' but has sensible break behaviour in R. Produces a single
 #' histogram but is mostly called internally for things like
@@ -7,14 +7,16 @@
 #' @name ggHist
 #' @param parameter The a vecotr of values that you want a histogram of.
 #' @param fill The colour to fill the bars with
-#' @param breaks The algorithgm to calculate breaks. Defaults to "scott", can also be "fandd"
+#' @param breaks The algorithgm to calculate breaks. Defaults to "scott", can
+#' also be "fandd"
 #' @param title The title of the plot, defaults to off.
 #' @return A histogram with nice ggplot aesthetics, and good bin widths.
 
-ggHist <- function(output, pars, fill = "dodgerblue", breaks = "scott", title = "") {
+ggHist <- function(output, pars, fill = "dodgerblue", breaks = "scott",
+                   title = "") {
   dat <- output[ ,pars]
   dat <- dat[!is.na(dat)]
-  
+
   if (breaks == "scott") {
     bwidth <- 3.5 * sd(dat) * length(dat) ^ -(1/3)
   } else if (breaks == "fandd") {
@@ -22,9 +24,10 @@ ggHist <- function(output, pars, fill = "dodgerblue", breaks = "scott", title = 
   } else {
     bwidth = (max(dat) - min(dat))/30
   }
-    
+
   if (fill == "count") {
-    z <- ggplot(data.frame(parameter = dat), aes(x = parameter, fill = ..count..)) +
+    z <- ggplot(data.frame(parameter = dat), aes(x = parameter,
+                                                 fill = ..count..)) +
       geom_histogram(color = "darkgray", binwidth = bwidth) +
       ggtitle(paste(pars, title))
   } else {
